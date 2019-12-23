3a5310dc2028a132a6ca044bc8422ed3bc72b357
l13262193815 <l13262193815@sina.com>
2019-12-23 17:30:09 +0800

    解决冲突

--- src/com/java/test/TestGit.java
+++ src/com/java/test/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("输出打印");
         System.out.println("更新源码");
+        System.out.println("解决冲突问题");
     }
 }
