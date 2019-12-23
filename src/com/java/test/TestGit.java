0d8d35788a179a67ad77f73c07608284702029db
l13262193815 <l13262193815@sina.com>
2019-12-23 22:26:24 +0800

    测试分支

--- src/com/java/test/TestGit.java
+++ src/com/java/test/TestGit.java
@@ -2,9 +2,6 @@ package com.java.test;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("输出打印");
-        System.out.println("更新源码");
-        System.out.println("解决冲突问题");
-        System.out.println("重新制造冲突");
+        System.out.println("分支测试");
     }
 }
